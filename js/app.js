b55c740e98968be1475b56dc23e7bf6158921701
catxii <catxii@qq.com>
2015-08-27 15:14:05 +0800

    提交欢迎页面&登陆页面

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
 //掌上订单脚本文件
+
+//login页面
+$(".show-btn").click(function(){
+	if(!$(this).hasClass("active")){
+		$("#password").attr("type","text");
+		$(this).addClass("active");
+	}else{
+		$(this).removeClass("active");
+		$("#password").attr("type","password");
+	}
+})
