210b92b48bcb73ba1d254eb83a07f2e3283e6a04
catxii <catxii@qq.com>
2015-08-27 22:33:24 +0800

    提交【app首页】

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 //掌上订单脚本文件
 
+
 //login页面
 $(".show-btn").click(function(){
 	if(!$(this).hasClass("active")){
@@ -11,4 +12,23 @@ $(".show-btn").click(function(){
 	}
 })
 
-$()
+$(document).on('click','.alert-text',function () {
+    $.alert('请输入用户名');
+ });
+ 
+$(document).on('click', '.alert-text-title-callback',function () {
+    $.alert('服务器正在抢修中...', '登陆异常', function () {
+        $.alert('你点击了确认按钮')
+    });
+});
+
+ $(document).on('click','.confirm-title-ok-cancel', function () {
+  $.confirm('删除信息不能恢复', '提示信息', 
+    function () {
+      $.alert('你点击了确定按钮');
+    },
+    function () {
+      $.alert('你点击了取消按钮');
+    }
+  );
+});
