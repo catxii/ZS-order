090cd4c5a2b78bb3fee2dcd19995d597618dfa0c
catxii <catxii@qq.com>
2015-10-23 11:46:13 +0800

    修改下拉刷新效果

--- js/app.js
+++ js/app.js
@@ -110,25 +110,25 @@ $(function() {
 
 // 添加'refresh'监听器
   //下拉刷新页面
-  $(document).on("pageInit", "#page-ptr", function(e, id, page) {
-    var $content = $(page).find(".content").on('refresh', function(e) {
-      // 模拟2s的加载过程
-      setTimeout(function() {
-        var cardHTML = '<div class="card">' +
-          '<div class="card-header">标题</div>' +
-          '<div class="card-content">' +
-          '<div class="card-content-inner">内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容内容' +
-          '</div>' +
-          '</div>' +
-          '</div>';
+$(document).on('refresh', '.pull-to-refresh-content',function(e) {
+		    // 模拟2s的加载过程
+		    setTimeout(function() {
+		        var cardNumber = $(e.target).find('.card').length + 1;
+		        var cardHTML = '<div class="card">' +
+		                          '<div class="card-header">card'+cardNumber+'</div>' +
+		                          '<div class="card-content">' +
+		                            '<div class="card-content-inner">' +
+		                                '这里是第' + cardNumber + '个card，下拉刷新会出现第' + (cardNumber + 1) + '个card。' +
+		                            '</div>' +
+		                          '</div>' +
+		                      '</div>';
 
-        $content.find('.card-container').prepend(cardHTML);
-        // 加载完毕需要重置
-        $.pullToRefreshDone($content);
-      }, 2000);
-    });
-  });
-
+		        $(e.target).find('.card-container').prepend(cardHTML);
+		        // 加载完毕需要重置
+		        $.pullToRefreshDone('.pull-to-refresh-content');
+		    }, 2000);
+		});
+$.initPullToRefresh(".pull-to-refresh-content")
 
   //调取时间
 
